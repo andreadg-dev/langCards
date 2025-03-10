b8001c5bf69aaff6ae5c2119b1b4e1188e651b5b
andreadg-dev <andydg88@gmail.com>
2025-03-10 10:42:49 +0100

    Add the dataSource.xlsx file and the Notes sheet. Adapt the ps1 file to add the funcionality to create the notes.json file as well

--- convertXlsxToJSON.ps1
+++ convertXlsxToJSON.ps1
@@ -1,5 +1,11 @@
-$path = Read-Host "Type your excel file path"
 Import-Module ImportExcel
-$sentences = Import-Excel -Path $path
+
+#Create the sentences json file
+$sentences = Import-Excel -Path ".\dataSource.xlsx" -WorksheetName "Sentences"
 $sentences | ForEach-Object { $_.ID = [int]$_.ID }
-$sentences | ConvertTo-Json | Out-File ".\sentences2.json"
+$sentences | ConvertTo-Json | Out-File ".\sentences.json"
+
+#Create the notes json file
+$notes = Import-Excel -Path ".\dataSource.xlsx" -WorksheetName "Notes"
+$notes | ForEach-Object { $_.ID = [int]$_.ID }
+$notes | ConvertTo-Json | Out-File ".\notes.json"
